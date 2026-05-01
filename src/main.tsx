c234df13512033992ab11f6ab09c624484a47249
Mark Barboze <markbarboze@gmail.com>
2026-05-01 18:19:37 +0530

    Remove Firestore test crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { testFirestoreConnection } from './lib/firebase.ts';
-
-// Initialize connection test
-testFirestoreConnection();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
