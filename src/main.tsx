c183e88fbd44cf91c043584f7d783d32a6f418ef
Mark Barboze <markbarboze@gmail.com>
2026-05-01 19:32:00 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
-import './lib/firebase';  // 🔥 MUST BE FIRST
+import './lib/firebase'; // MUST BE FIRST
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
